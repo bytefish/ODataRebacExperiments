8752e9dec19f995294fa1e8c57f5ef8458a90abf
Philipp Wagner <pwag86@googlemail.com>
2023-11-09 11:46:00 +0100

    Remove useless ConfigureAwait

--- src/Blazor/RebacExperiments.Blazor/Pages/UserTasksDataGrid.razor.cs
+++ src/Blazor/RebacExperiments.Blazor/Pages/UserTasksDataGrid.razor.cs
@@ -114,7 +114,7 @@ namespace RebacExperiments.Blazor.Pages
                 {
                     request.QueryParameters.Orderby = new[] { parameters.OrderBy };
                 }
-            }).ConfigureAwait(false);    
+            });    
         }
     }
 }
