e49df6935002170ff1732874d5b92b838fb74809
Philipp Wagner <pwag86@googlemail.com>
2023-11-09 11:56:00 +0100

    Remove obsolete assignments

--- src/Blazor/RebacExperiments.Blazor.Shared/Models/ODataQueryParameters.cs
+++ src/Blazor/RebacExperiments.Blazor.Shared/Models/ODataQueryParameters.cs
@@ -12,22 +12,22 @@ namespace RebacExperiments.Blazor.Shared.Models
         /// <summary>
         /// Gets or sets the number of elements to skip.
         /// </summary>
-        public int? Skip { get; set; } = null;
+        public int? Skip { get; set; }
 
         /// <summary>
         /// Gets or sets the number of elements to take.
         /// </summary>
-        public int? Top { get; set; } = null;
+        public int? Top { get; set; }
 
         /// <summary>
         /// Gets or sets the filter clause.
         /// </summary>
-        public string? Filter { get; set; } = null;
+        public string? Filter { get; set; }
 
         /// <summary>
         /// Gets or sets the order by clause.
         /// </summary>
-        public string? OrderBy { get; set; } = null;
+        public string? OrderBy { get; set; }
 
         /// <summary>
         /// Gets or sets the option to include the count (default: <see cref="true"/>).
