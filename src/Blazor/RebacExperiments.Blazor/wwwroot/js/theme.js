56568dbca3fa14ff0eb22d0c467d3131b8025bd9
Philipp Wagner <pwag86@googlemail.com>
2023-11-19 11:00:47 +0100

    Fix NullReference Exception in theme.js script, Fix PageTitle and Header

--- src/Blazor/RebacExperiments.Blazor/wwwroot/js/theme.js
+++ src/Blazor/RebacExperiments.Blazor/wwwroot/js/theme.js
@@ -40,12 +40,12 @@ export function getThemeCookieValue() {
 
 export function switchHighlightStyle(dark) {
     if (dark) {
-        document.querySelector(`link[title="dark"]`).removeAttribute("disabled");
-        document.querySelector(`link[title="light"]`).setAttribute("disabled", "disabled");
+        document.querySelector(`link[title="dark"]`)?.removeAttribute("disabled");
+        document.querySelector(`link[title="light"]`)?.setAttribute("disabled", "disabled");
     }
     else {
-        document.querySelector(`link[title="light"]`).removeAttribute("disabled");
-        document.querySelector(`link[title="dark"]`).setAttribute("disabled", "disabled");
+        document.querySelector(`link[title="light"]`)?.removeAttribute("disabled");
+        document.querySelector(`link[title="dark"]`)?.setAttribute("disabled", "disabled");
     }
 }
 
